cf932b18cacce6afdbc88d20bc8e4b280c4e1d52
Ran Reichman <ran@flarion.io>
2024-10-27 14:17:42 +0200

    rebase

--- crates/polars-core/src/chunked_array/ops/lambda.rs
+++ crates/polars-core/src/chunked_array/ops/lambda.rs
@@ -145,6 +145,12 @@ impl LambdaExpression {
                 }
                 otherwise.eval_numeric::<T>(args)
             }
+            LambdaExpression::Length(expr) => {
+                match expr.eval_numeric::<T>(args) {
+                    AnyValue::Null => AnyValue::Null,
+                    _ => AnyValue::Int64(1)
+                }
+            }
         }
     }
 
@@ -205,6 +211,12 @@ impl LambdaExpression {
                     }
                 }
                 otherwise.eval_bool(args)
+            }
+            LambdaExpression::Length(expr) => {
+                match expr.eval_bool(args) {
+                    AnyValue::Null => AnyValue::Null,
+                    _ => AnyValue::Int64(1)
+                }
             }
         }
     }
