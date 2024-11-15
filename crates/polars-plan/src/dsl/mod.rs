41d1c83784cf967217c23a8961d82b90b74e5f9a
Emily Matheys <emily.matheys@flarion.io>
2024-11-15 14:18:25 +0200

    updated docs

--- crates/polars-plan/src/dsl/mod.rs
+++ crates/polars-plan/src/dsl/mod.rs
@@ -1953,9 +1953,6 @@ impl Expr {
     }
 
     /// This is essentially the apply() function, but sets the appropriate flags to use with an aggregate buffer
-    /// ALLOW_GROUP_AWARE - lets us call this apply function in a group_by context
-    /// RETURNS_SCALAR - Since we return a single Binary buffer, we need this, otherwise it would wrap the results in a Series
-    /// CHANGES_LENGTH - This function possibly changes the length of the Series(returns a length of 1), so we need this flag
     pub fn flarion_aggregate<F>(self, function: F, output_type: GetOutput) -> Self
     where
         F: Fn(Series) -> PolarsResult<Option<Series>> + 'static + Send + Sync,
@@ -1968,9 +1965,9 @@ impl Expr {
             output_type,
             options: FunctionOptions {
                 collect_groups: ApplyOptions::GroupWise,
-                flags: FunctionFlags::ALLOW_GROUP_AWARE
-                    | FunctionFlags::RETURNS_SCALAR
-                    | FunctionFlags::CHANGES_LENGTH,
+                flags: FunctionFlags::ALLOW_GROUP_AWARE // lets us call this apply function in a group_by context
+                    | FunctionFlags::RETURNS_SCALAR // Since we return a single Binary buffer, we need this, otherwise it would wrap the results in a Series
+                    | FunctionFlags::CHANGES_LENGTH, // This function possibly changes the length of the Series(returns a length of 1), so we need this flag
                 ..Default::default()
             },
         }
