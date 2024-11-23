ebfc708b0eec8326797f2dee29c562d944ff2174
Emily Matheys <emily.matheys@flarion.io>
2024-11-23 13:30:35 +0200

    fix append

--- crates/polars-arrow/src/io/ipc/append/mod.rs
+++ crates/polars-arrow/src/io/ipc/append/mod.rs
@@ -66,6 +66,7 @@ impl<R: Read + Seek + Write> FileWriter<R> {
                 cannot_replace: true,
             },
             encoded_message: Default::default(),
+            total_written: offset as usize,
         })
     }
 }
