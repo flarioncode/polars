4d836e606da2e81bce691ab8bf8f60cab62de351
Ran Reichman <ran@flarion.io>
2024-12-11 17:42:09 +0200

    attempt to cover window and implode expressions

--- crates/polars-mem-engine/src/executors/projection_utils.rs
+++ crates/polars-mem-engine/src/executors/projection_utils.rs
@@ -247,6 +247,8 @@ pub(super) fn evaluate_physical_expressions(
 fn is_reducing_expr(phys_expr: &dyn PhysicalExpr) -> bool {
     if let Some(expr) = phys_expr.as_expression() {
         match expr {
+            Expr::Window { .. } => false,
+            Expr::Agg(AggExpr::Implode(_)) => false,
             // Only match pure aggregations that reduce cardinality
             Expr::Agg(agg) => matches!(
                 agg,
