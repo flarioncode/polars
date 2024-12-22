335a83f783b0b3dcd5c5b22177f468e047280c54
Emily Matheys <emily.matheys@flarion.io>
2024-12-22 17:55:16 +0200

    don't do unchecked if we can help it

--- crates/polars-arrow/src/compute/cast/binview_to.rs
+++ crates/polars-arrow/src/compute/cast/binview_to.rs
@@ -86,7 +86,9 @@ where
     let is_float = T::is_float();
 
     // Attempt to interpret input as UTF-8 and trim invalid or whitespace characters
-    let x = unsafe { std::str::from_utf8_unchecked(x).trim().as_bytes() };
+    let x = std::str::from_utf8(x)
+        .map(|s| s.trim().as_bytes())
+        .unwrap_or_else(|_| x.trim_ascii());
 
     // Extract and process leading sign
     let (x, is_negative) = match x.first() {
