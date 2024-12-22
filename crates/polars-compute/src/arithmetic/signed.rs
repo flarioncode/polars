5ed3d572e5a67f8d37dc4ea8835626d8ed6893a3
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-12-22 13:51:40 +0200

    fix reminder with negative numbers for signed numbers when rhs is scalar

--- crates/polars-compute/src/arithmetic/signed.rs
+++ crates/polars-compute/src/arithmetic/signed.rs
@@ -199,29 +199,29 @@ macro_rules! impl_signed_arith_kernel {
                 } else {
                     #[cfg(feature = "consistent_arithmetic")]
                     {
-                         prim_unary_values(lhs, |x| x % rhs)
+                        prim_unary_values(lhs, |x| x % rhs)
                     }
                     #[cfg(not(feature = "consistent_arithmetic"))]
                     {
                         let scalar_u = rhs.unsigned_abs();
                         let red = <$StrRed>::new(scalar_u);
                         prim_unary_values(lhs, |x| {
-                                // Remainder fits in signed type after reduction.
-                                // Largest possible modulo -I::MIN, with
-                                // -I::MIN-1 == I::MAX as largest remainder.
-                                let mut rem_u = x.unsigned_abs() % red;
+                            // Remainder fits in signed type after reduction.
+                            // Largest possible modulo -I::MIN, with
+                            // -I::MIN-1 == I::MAX as largest remainder.
+                            let mut rem_u = x.unsigned_abs() % red;
 
-                                // Mixed signs: swap direction of remainder.
-                                if rem_u != 0 && (rhs < 0) != (x < 0) {
-                                    rem_u = scalar_u - rem_u;
-                                }
+                            // Mixed signs: swap direction of remainder.
+                            if rem_u != 0 && (rhs < 0) != (x < 0) {
+                                rem_u = scalar_u - rem_u;
+                            }
 
-                                // Remainder should have sign of RHS.
-                                if rhs < 0 {
-                                    -(rem_u as $T)
-                                } else {
-                                    rem_u as $T
-                                }
+                            // Remainder should have sign of RHS.
+                            if rhs < 0 {
+                                -(rem_u as $T)
+                            } else {
+                                rem_u as $T
+                            }
                         })
                     }
                 }
