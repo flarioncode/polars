fc03d3ec1bbe2c2daf910cdcbf849a3cca213a37
Emily Matheys <emily.matheys@flarion.io>
2025-01-16 12:21:27 +0200

    remove featuregate for sink function

--- crates/polars-lazy/src/frame/mod.rs
+++ crates/polars-lazy/src/frame/mod.rs
@@ -930,13 +930,6 @@ impl LazyFrame {
         )
     }
 
-    #[cfg(any(
-        feature = "ipc",
-        feature = "parquet",
-        feature = "cloud_write",
-        feature = "csv",
-        feature = "json",
-    ))]
     fn sink(mut self, payload: SinkType, msg_alternative: &str) -> Result<(), PolarsError> {
         self.opt_state |= OptFlags::STREAMING;
         self.logical_plan = DslPlan::Sink {
